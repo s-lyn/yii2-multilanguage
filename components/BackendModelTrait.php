efc3c3e9ed8bc50326889697638aa279687dd5d0
evye <evye@evye.evye>
2016-06-04 14:20:31 +0300

    Избавление от дефолтного кеша

--- components/BackendModelTrait.php
+++ components/BackendModelTrait.php
@@ -5,33 +5,14 @@ use Yii;
 
 trait BackendModelTrait {
     
-    public function getContentCacheDependencyKey() {
-        
-        $key = 'id';
-        if (property_exists($this, 'contentCacheDependency') && $this->contentCacheDependency) {
-            $key = $this->contentCacheDependency;
-        }
-        return $this->{$key};
-    }
-    
     /**
+     * Returns list af all language versions
      * @return \yii\db\ActiveQuery
      */
     public function getContentAll() {
         $contentModelName = $this->getContentModelName();
         if ($contentModelName)
             return $this->hasMany($contentModelName::className(), ['parent_id' => 'id']);
-    }
-    
-    public function afterSave($insert, $changedAttributes) {
-        parent::afterSave($insert, $changedAttributes);
-        if (!$insert) {
-            $cache = Yii::$app->cache;
-            foreach (Yii::$app->params['languages'] as $lang_id => $lang) {
-                $cacheKey = static::getContentCacheKey($this->getContentCacheDependencyKey(), $lang_id);
-                $res = $cache->delete($cacheKey);
-            }
-        }
     }
     
 }
