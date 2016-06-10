c09ee6efb9ee49f7cfa03526f58c1cec89753771
pjhl <pjhl.coder@gmail.com>
2016-06-10 16:26:46 +0300

    Fix generate internal links directory

--- components/AdvancedUrlManager.php
+++ components/AdvancedUrlManager.php
@@ -5,38 +5,49 @@ namespace pjhl\multilanguage\components;
 use Yii;
 use yii\web\UrlManager;
 use pjhl\multilanguage\helpers\Languages;
+use pjhl\multilanguage\LangHelper;
 
 class AdvancedUrlManager extends UrlManager {
-    
+
     public function createUrl($params) {
-        
+
         $default = Languages::all()->getConfigDefault();
-        $current = Languages::all()->getConfigCurrent();
+        $current = LangHelper::getLanguageByParam('locale', Yii::$app->language);
         
-        $url_prefix = null;
-        
+        // Url's language such as "en"
+        $url_lang = null;
+
         if (isset($params['x-language-url']) && !$params['x-language-url']) {
             $params['x-language-url'] = $default['url'];
         }
-        
+
         if (isset($params['x-language-url']) && Languages::all()->getConfigByParam('url', $params['x-language-url'])) {
             $isDefault = isset($default['default']) && $default['default'];
-            $url_prefix = ($params['x-language-url'] === $default['url'] && $isDefault)
-                    ? ''
-                    : $params['x-language-url'];
-            unset($params['x-language-url']);
+            $url_lang = ($params['x-language-url'] === $default['url'] && $isDefault) ? '' : $params['x-language-url'];
+            // Delete if is pretty url
+            if ($this->enablePrettyUrl) {
+                unset($params['x-language-url']);
+            }
         }
-        
-        if ($url_prefix === null) {
+
+        if ($url_lang === null) {
             // Иначе подставляем текущий язык
-            $url_prefix = Yii::$app->request->getLanguageInurl();
+            $url_lang = Yii::$app->request->getLanguageInurl();
         }
-        
+
         $url = parent::createUrl($params);
-        if ($url_prefix) {
-            $url = '/' . $url_prefix . $url;
+        if ($url_lang) {
+            // Pretty url
+            if ($this->enablePrettyUrl && !$this->showScriptName) {
+                $pattern = "/^" . preg_quote($this->baseUrl, '/') . "/";
+                $url = preg_replace($pattern, $this->baseUrl . '/' . $url_lang, $url );
+            } else if ($this->enablePrettyUrl && $this->showScriptName) {
+                // Pretty url with showScriptName 
+                $pattern = "/^" . preg_quote($this->scriptUrl, '/') . "/";
+                $url = preg_replace($pattern, $this->scriptUrl . '/' . $url_lang , $url );
+            }
         }
         return $url;
     }
-    
+
 }
